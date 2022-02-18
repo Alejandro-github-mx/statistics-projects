8d60dc07bcf2e08f0d0a584e9fc82ee5c79805e8
Alejandro-github-mx <72238862+Alejandro-github-mx@users.noreply.github.com>
2022-02-18 23:16:09 +0100

    Start to adding final comments and analysis.

--- 2022-02-18-tarea-phd-statistics/2022-final-Homework-AGR.R
+++ 2022-02-18-tarea-phd-statistics/2022-final-Homework-AGR.R
@@ -1,13 +1,27 @@
+# 1. Importing the dataset
+
 nitrox <- read.csv("../2022-02-18-tarea-phd-statistics/2019-1971-nitrous-TRANSPOSED-Poland.csv")
 crops <- read.csv("../dataCropsTransposed.csv")
+
+# 2. Checking crops dataset characteristics
 View(crops)
 colnames(crops)
 
+# 3. Creating the dataset I will work with
 finalCrops <- crops[,c("Cereals..Total", 
                        "Roots.and.Tubers..Total",
                        "Potatoes")]
 View(finalCrops)
 
+# 5. Merging Nitrox & finalCrops datasets
 CropsNitrox <- cbind(nitrox, finalCrops)
 View(CropsNitrox)
+
+# 6. Summary of CropsNitrox dataset
 summary(CropsNitrox)
+
+
+# 7. Exporting CropsNitrox dataset
+write.csv(CropsNitrox, 
+          "C:\\Users\\USER\\Documents\\Desarrollador\\PYTHON\\2021-Python-exercises\\statistics-projects\\CropsNitrox.csv",
+          row.names = TRUE)
