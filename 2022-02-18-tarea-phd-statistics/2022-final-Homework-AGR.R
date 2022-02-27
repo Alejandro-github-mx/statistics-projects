6f2f685c38bc5ab906031fa0a0486cf03f910e2b
Alejandro-github-mx <72238862+Alejandro-github-mx@users.noreply.github.com>
2022-02-27 17:10:49 +0100

    Final conslussiones/discussions.

--- 2022-02-18-tarea-phd-statistics/2022-final-Homework-AGR.R
+++ 2022-02-18-tarea-phd-statistics/2022-final-Homework-AGR.R
@@ -1,27 +1,32 @@
-# 1. Importing the dataset
+# Script for Crops dataset merge
+## Akademia Leona Kozminskiego
+### Class: Quantitative Methods
+### Student: Alejandro Guzmán Rivera (23-DS)
+
+## 1. Import the dataset
 
 nitrox <- read.csv("../2022-02-18-tarea-phd-statistics/2019-1971-nitrous-TRANSPOSED-Poland.csv")
 crops <- read.csv("../dataCropsTransposed.csv")
 
-# 2. Checking crops dataset characteristics
+## 2. Check crops dataset characteristics
 View(crops)
 colnames(crops)
 
-# 3. Creating the dataset I will work with
+## 3. Create the dataset I will work with
 finalCrops <- crops[,c("Cereals..Total", 
                        "Roots.and.Tubers..Total",
                        "Potatoes")]
 View(finalCrops)
 
-# 5. Merging Nitrox & finalCrops datasets
+## 5. Merge Nitrox & finalCrops datasets
 CropsNitrox <- cbind(nitrox, finalCrops)
 View(CropsNitrox)
 
-# 6. Summary of CropsNitrox dataset
+## 6. Summary of CropsNitrox dataset
 summary(CropsNitrox)
 
 
-# 7. Exporting CropsNitrox dataset
+## 7. Exporte CropsNitrox dataset
 write.csv(CropsNitrox, 
           "C:\\Users\\USER\\Documents\\Desarrollador\\PYTHON\\2021-Python-exercises\\statistics-projects\\CropsNitrox.csv",
           row.names = TRUE)
